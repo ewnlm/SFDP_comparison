24f8f6dca1d644a7623a79df8457a3c5d06bd1ae
ewnlm <luckjimmy829@gmail.com>
2016-08-06 22:07:17 +0800

    multi-files comparison

---
+++ list_cmp_sfdp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+  bash compare_sfdp.sh -g golden.csv -v verify.txt
+  bash compare_sfdp.sh -g golden.csv -v verify_001.txt
+  bash compare_sfdp.sh -g golden.csv -v verify_002.txt
+  bash compare_sfdp.sh -g golden.csv -v verify_003.txt
+  bash compare_sfdp.sh -g golden.csv -v verify_004.txt
