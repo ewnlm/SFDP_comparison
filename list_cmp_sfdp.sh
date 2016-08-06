7619d24e1c184392f21a242c7dc37842f50cb6b2
ewnlm <luckjimmy829@gmail.com>
2016-08-06 22:28:23 +0800

    for multi-files

--- list_cmp_sfdp.sh
+++ list_cmp_sfdp.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-  bash compare_sfdp.sh -g golden.csv -v verify.txt
-  bash compare_sfdp.sh -g golden.csv -v verify_001.txt
-  bash compare_sfdp.sh -g golden.csv -v verify_002.txt
-  bash compare_sfdp.sh -g golden.csv -v verify_003.txt
-  bash compare_sfdp.sh -g golden.csv -v verify_004.txt
+  for file in `ls verify*txt`
+  do
+    bash compare_sfdp.sh -g golden.csv -v ${file}
+  done
